0767d615af44e21c32edeb89c320e7dd70e41b08
Anwesh43 <anweshthecool0@gmail.com>
2021-06-07 01:35:17 +0530

    using activity in view

--- app/src/main/java/com/example/linkedballsweeptonextview/MainActivity.kt
+++ app/src/main/java/com/example/linkedballsweeptonextview/MainActivity.kt
@@ -4,11 +4,12 @@ import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import com.example.ballsweeponextview.BallSweepToNextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BallSweepToNextView.create(this)
         fullScreen()
     }
 }
