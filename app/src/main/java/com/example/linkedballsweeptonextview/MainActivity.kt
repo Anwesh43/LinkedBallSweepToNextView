e8587146beea952ef342ecf29b046c87f83008f9
Anwesh43 <anweshthecool0@gmail.com>
2021-06-07 01:15:31 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedballsweeptonextview/MainActivity.kt
+++ app/src/main/java/com/example/linkedballsweeptonextview/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.linkedballsweeptonextview
 
+import android.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
