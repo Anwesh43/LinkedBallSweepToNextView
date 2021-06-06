c0f5559a0d2b57ed752b8278db903523cee46b25
Anwesh43 <anweshthecool0@gmail.com>
2021-06-07 00:43:37 +0530

    adding BallSweepToNextView class

--- ballsweeponextview/src/main/java/com/example/ballsweeponextview/BallSweepToNextView.kt
+++ ballsweeponextview/src/main/java/com/example/ballsweeponextview/BallSweepToNextView.kt
@@ -55,3 +55,19 @@ fun Canvas.drawBSTNNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBallSweepToNext(scale, w, h, paint)
 }
+
+class BallSweepToNextView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
