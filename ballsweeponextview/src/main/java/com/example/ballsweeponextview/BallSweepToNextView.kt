fe4b3b5b020d1b2119dbea71f7247d005089e0ea
Anwesh43 <anweshthecool0@gmail.com>
2021-06-07 01:07:18 +0530

    adding Renderer class

--- ballsweeponextview/src/main/java/com/example/ballsweeponextview/BallSweepToNextView.kt
+++ ballsweeponextview/src/main/java/com/example/ballsweeponextview/BallSweepToNextView.kt
@@ -183,4 +183,27 @@ class BallSweepToNextView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BallSweepToNextView) {
+
+        private val animator : Animator = Animator(view)
+        private val bstn : BallSweepToNext = BallSweepToNext(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            bstn.draw(canvas, paint)
+            animator.animate {
+                bstn.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            bstn.startUpdating {
+                animator.start()
+            }
+        }
+    }
 }
