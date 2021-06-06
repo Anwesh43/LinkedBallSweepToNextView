4b4ee05caeffd0e234af7ec013b86a2a95117a80
Anwesh43 <anweshthecool0@gmail.com>
2021-06-07 00:41:58 +0530

    adding Canvas extension methods

--- ballsweeponextview/src/main/java/com/example/ballsweeponextview/BallSweepToNextView.kt
+++ ballsweeponextview/src/main/java/com/example/ballsweeponextview/BallSweepToNextView.kt
@@ -30,3 +30,28 @@ val rot : Float = 90f
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+
+fun Canvas.drawBallSweepToNext(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / lFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sc4 : Float = scale.divideScale(3, parts)
+    val sc5 : Float = scale.divideScale(4, parts)
+    save()
+    translate(w / 2, h / 2)
+    rotate(rot * sc3)
+    drawCircle(size + (w / 2 - size + r) * sc4, -r + (h / 2 + r) * sc2, r, paint)
+    drawLine(0f, 0f, size * (sc1 - sc5), 0f, paint)
+    restore()
+}
+
+fun Canvas.drawBSTNNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()\
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBallSweepToNext(scale, w, h, paint)
+}
