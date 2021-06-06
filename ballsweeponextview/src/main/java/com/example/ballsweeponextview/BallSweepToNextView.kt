d3325cce1ed2e30fd65925839c29e16c96200647
Anwesh43 <anweshthecool0@gmail.com>
2021-06-07 00:33:35 +0530

    adding constants and libraries

--- ballsweeponextview/src/main/java/com/example/ballsweeponextview/BallSweepToNextView.kt
+++ ballsweeponextview/src/main/java/com/example/ballsweeponextview/BallSweepToNextView.kt
@@ -1,2 +1,28 @@
 package com.example.ballsweeponextview
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.graphics.RectF
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#673AB7",
+    "#00C853",
+    "#304FFE",
+    "#BF360C"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val delay : Long = 90
+val lFactor : Float = 4.9f
+val rFactor : Float = 11.2f
+val strokeFactor : Float = 90f
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rot : Float = 90f
